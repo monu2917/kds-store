b85e35a17836f4b18c05485e6adc05530ab41c81
MONU <156662903+monu2917@users.noreply.github.com>
2024-12-24 22:49:57 +0530

    html.css ,javascript

---
+++ java.js
@@ -0,0 +1,162 @@
+
+let DBdata=[
+    {
+        id:"Screen Size - 15.6 Inches",
+        title:"HP (16GB RAM/512GB SSD/FHD/15.6",
+        brand:" Brand : Dell",
+        price:"₹17,000 (4% off)",
+        pic:"https://imgs.search.brave.com/_HZoNx5IthN7o30H3SVygilNVPCtywmaWePrPkZHngk/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTUz/MDcyMTM5L3Bob3Rv/L2EtaHAtZG00LTMw/MDBlYS1sYXB0b3At/cGhvdG9ncmFwaGVk/LWR1cmluZy1hLXN0/dWRpby1zaG9vdC1m/b3Itb2ZmaWNpYWwt/d2luZG93cy1tYWdh/emluZS5qcGc_cz02/MTJ4NjEyJnc9MCZr/PTIwJmM9eW4tcDhw/Q25QQy1oRUQtdS1V/U3NpcVFna1RGMWho/RFNfMkxjT0habkJM/az0",
+        button:"ADD TO CART",
+        button:"BUY"
+    }
+    ,
+    {
+        id:" Screen Size - 15.6 Inches",
+        title:"HP (16GB RAM/512GB SSD/FHD/15.6",
+        brand:" Brand:HP",
+        price:"₹17,000 (4% off)",
+        pic:"https://imgs.search.brave.com/3YruqyZt5KrqNLKHoFHDTnBy2U-5M59_RxRJaXMABco/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9jZG4u/bW9zLmNtcy5mdXR1/cmVjZG4ubmV0L2ZB/S0tQZlRxYlRSQ3JG/Q2lwWFhZQU4tMzIw/LTgwLmpwZWc",
+        button:"ADD TO CART",
+        button:"BUY"
+
+    },
+    {
+        id:" Screen Size - 15.6 Inches",
+        title:"HP (16GB RAM/512GB SSD/FHD/15.6",
+        brand:"Brand :Lenovo",
+        price:"₹17,000 (4% off)",
+        pic:"https://imgs.search.brave.com/bQNI9HFRzABLiE47ivVZf8zjE9o-B6Uqa0wKh23CWeU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTM4/MjA1OTI3L3Bob3Rv/L2xlbm92by1pZGVh/cGFkLXo1NjAtbGFw/dG9wLWJhdGgtZmVi/cnVhcnktMjUtMjAx/MS5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9bmVfX2hoRzkt/ZGtwRlUzWkljUDlD/bUJFbm5VY1pDR2F4/N2JjVGdka2ZMOD0",
+        button:"ADD TO CART",
+        button:"BUY"
+    },  
+
+    {
+        id:" Screen Size - 15.6 Inches",
+        title:"HP (16GB RAM/512GB SSD/FHD/15.6",
+        brand:"Brand : Ashus",
+        price:"₹17,000 (4% off)",
+        pic:"https://imgs.search.brave.com/_z_fnt49Yts2p3CY3qzd8bX-60TBC0RGfIMy1Gk4c-M/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5naXRlbS5jb20v/cGltZ3MvbS8zNDIt/MzQyNjI4N19kZWxs/LWxhcHRvcC1wbmct/dHJhbnNwYXJlbnQt/aW1hZ2UtZGVsbC1s/YXB0b3AtaW1hZ2Uu/cG5n",
+        button:"ADD TO CART",
+        button:"BUY"
+    }
+    ,
+    {
+        id:" Screen Size - 15.6 Inches",
+        title:"HP (16GB RAM/512GB SSD/FHD/15.6",
+        brand:"Brand : Latitude",
+        price:"₹17,000 (4% off)",
+        pic:"https://imgs.search.brave.com/bQNI9HFRzABLiE47ivVZf8zjE9o-B6Uqa0wKh23CWeU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTM4/MjA1OTI3L3Bob3Rv/L2xlbm92by1pZGVh/cGFkLXo1NjAtbGFw/dG9wLWJhdGgtZmVi/cnVhcnktMjUtMjAx/MS5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9bmVfX2hoRzkt/ZGtwRlUzWkljUDlD/bUJFbm5VY1pDR2F4/N2JjVGdka2ZMOD0",
+        button:"ADD TO CART",
+        button:"BUY"
+    },  
+
+    {
+        id:" Screen Size - 15.6 Inches",
+        title:"HP (16GB RAM/512GB SSD/FHD/15.6",
+        brand:" Brand :DELL ",
+        price:"₹17,000 (4% off)",
+        pic:"https://imgs.search.brave.com/_z_fnt49Yts2p3CY3qzd8bX-60TBC0RGfIMy1Gk4c-M/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5naXRlbS5jb20v/cGltZ3MvbS8zNDIt/MzQyNjI4N19kZWxs/LWxhcHRvcC1wbmct/dHJhbnNwYXJlbnQt/aW1hZ2UtZGVsbC1s/YXB0b3AtaW1hZ2Uu/cG5n",
+        button:"ADD TO CART",
+        button:"BUY"
+    }
+    ,
+    {
+        id:" Screen Size - 15.6 Inches",
+        title:"HP (16GB RAM/512GB SSD/FHD/15.6",
+        brand:" Model:HP 4450",
+        price:"₹17,000 (4% off)",
+        pic:"https://imgs.search.brave.com/bQNI9HFRzABLiE47ivVZf8zjE9o-B6Uqa0wKh23CWeU/rs:fit:500:0:0:0/g:ce/aHR0cHM6Ly9tZWRp/YS5nZXR0eWltYWdl/cy5jb20vaWQvMTM4/MjA1OTI3L3Bob3Rv/L2xlbm92by1pZGVh/cGFkLXo1NjAtbGFw/dG9wLWJhdGgtZmVi/cnVhcnktMjUtMjAx/MS5qcGc_cz02MTJ4/NjEyJnc9MCZrPTIw/JmM9bmVfX2hoRzkt/ZGtwRlUzWkljUDlD/bUJFbm5VY1pDR2F4/N2JjVGdka2ZMOD0",
+        button:"ADD TO CART",
+        button:"BUY"
+    },  
+
+    {
+        id:" Screen Size - 15.6 Inches",
+        title:"HP (16GB RAM/512GB SSD/FHD/15.6",
+        brand:" Model:HP 4034",
+        price:"₹17,000 (4% off)",
+        pic:"https://imgs.search.brave.com/_z_fnt49Yts2p3CY3qzd8bX-60TBC0RGfIMy1Gk4c-M/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5naXRlbS5jb20v/cGltZ3MvbS8zNDIt/MzQyNjI4N19kZWxs/LWxhcHRvcC1wbmct/dHJhbnNwYXJlbnQt/aW1hZ2UtZGVsbC1s/YXB0b3AtaW1hZ2Uu/cG5n",
+        button:"ADD TO CART",
+        button:"BUY"
+    }
+    
+]
+
+
+
+
+//  into card insert attributes 
+let title=document.createElement("h4")
+// console.log(title);
+
+
+
+// insert into home section 
+let texts=document.createElement("div")
+console.log(texts);
+texts.setAttribute("class","content")
+
+let productswraperEle=document.querySelector("#productswraper")
+let div= document.querySelector("#home")
+let h1 =document.createElement("h1")
+h1.textContent="WELCOME TO KDS LAPTOP STORE"
+let h3=document.createElement("h3")
+h3.textContent="Mr.Salim Ahmnd"
+let p=document.createElement("p")
+let p1=document.createElement("p")
+let btn2=document.createElement("button")
+let btn3=document.createElement("button")
+
+btn2.textContent="WHATSAPP"
+btn3.textContent="CONTECT US"
+p.textContent="All  Company(Dell , hp , Lenova , Samsang) laptop purchase and repair for contact  me"
+p1.textContent="Near Krishna Hospital Prem Nagar Hansi Hisar-125033"
+p.setAttribute("class","about");
+p1.setAttribute("class","address")
+btn2.setAttribute("class","whats")
+btn3.setAttribute("class","number")
+
+console.log(div);
+DBdata.map((product)=>{
+console.log(product);
+// todo > CREATEING ELEMENTS DYNAMICALLY
+let card=document.createElement("div")
+let image=document.createElement("img")
+let title = document.createElement("h3")
+let brand=document.createElement("h3")
+let id=document.createElement("p")
+let price=document.createElement("p")
+let addtocart=document.createElement("button")
+let BUY=document.createElement("button")
+title.setAttribute("class","title")
+brand.setAttribute("class","brnad")
+id.setAttribute("class","srno")
+price.setAttribute("class","price")
+BUY.setAttribute("class","buy")
+addtocart.setAttribute("class","addtocartbtn")
+
+title.textContent = product.title
+brand.textContent=product.brand
+id.textContent=product.id
+price.textContent=product.price
+addtocart.textContent="ADD TO CART"
+BUY.textContent="BUY"
+
+
+
+
+//TODO > SETTING ATTRIBUTE TO ELEMENTS DYNAMCALLY
+card.setAttribute("class","card")
+image.setAttribute("src",product.pic)
+//! APPENDING ALL ELEMENST AS A CHILD OF CARD
+card.append(image,title,brand,id,price,addtocart,BUY)
+div.append(texts)
+texts.append(h1,h3,p,p1,btn2,btn3)
+
+
+console.log(card);
+//! APPENDING CARD AS A CHILD OF PRODUCTS_WRAPPER
+productswraperEle.append(card)
+})
+
+//  footer prperty 
